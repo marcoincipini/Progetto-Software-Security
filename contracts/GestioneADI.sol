451ee5ab021bc6af1164759f13a9498784ffc8dc
Marco Incipini <marco.incipini@gmail.com>
2024-03-12 12:31:19 +0100

    Strutture dati commentate

--- contracts/GestioneADI.sol
+++ contracts/GestioneADI.sol
@@ -3,41 +3,46 @@ pragma solidity ^0.8.4;
 
 contract GestioneADI {
     // Attributi
+    // Struttura informazioni del paziente (hash)
     struct paziente {
         address pz;
         uint8 lat;
         uint8 lon;
     }
 
+    // Struttura informazioni attrezzature (hash)
     struct attrezzature {
         address paziente;
         string attrezzatura; // Valutare se possibile bytes32
     }
 
+    // Struttura informazioni terapie (hash)
     struct terapie {
         address paziente;
         string terapia; // Valutare se possibile bytes32
     }
 
+    // Struttura gestione delle conferme
     struct conferme {
         address paziente;
         address operatore;
         string prestazione;
     }
 
-    paziente[] private pazienti;
-    address[] private medici;
-    address[] private operatori;
-    address private asur;
-    attrezzature[] private ckattrezzatura;
-    terapie[] private ckterapie;
-    conferme[] private conferma;
+    paziente[] private pazienti;            // Array dei pazienti
+    address[] private medici;               // Array dei medici
+    address[] private operatori;            // Array degli operatori sanitari
+    address private asur;                   // Account amministratore
+    attrezzature[] private ckattrezzatura;  // Array delle attrezzature (hash)
+    terapie[] private ckterapie;            // Array delle terapie (1 per paziente) (hash)
+    conferme[] private conferma;            // Array gestione delle conferme
 
-    mapping(address => address) private medicoBase;
+    // Informazioni su chi può inserire prestazioni per un determinato paziente
+    mapping(address => address) private medicoBase; // Associazione paziente-medico (chiave-valore)
 
     // Inserire tutti i require necessari
-    // Creare una funzione che controlli se un elemento è presente nell'array
 
+    // Setter di tutte le strutture
     function setPaziente(address _pz, uint8 _lat, uint8 _lon) public {
         pazienti.push(paziente(_pz, _lat, _lon));
     }
@@ -66,16 +71,33 @@ contract GestioneADI {
         conferma.push(conferme(_pz, _op, _prest));
     }
 
+    // Remove della conferma (prestazione validata dal paziente)
     function removeConferma(uint256 _i) public{
         require(_i < conferma.length, "indice inesistente!!");
 
         delete conferma[_i];
     }
 
+    // Setter del mapping paziente-medico
     function setMedicobase(address _pz, address _md) public{
-        require(_pz, "Paziente inesistente");
-        require(_md, "Medico inesistente");
+        require(ckpaziente(_pz), "Paziente inesistente");
+        require(ckmedico(_md), "Medico inesistente");
 
         medicoBase[_pz] = _md;
+    }
+
+    // Controlli sulla presenza di un valore negli array
+    function ckpaziente(address _pz) private view returns (bool){
+        for (uint256 i=0; i < pazienti.length; i++){
+            if (pazienti[i].pz == _pz){return true;}
+        }
+        return false;
+    }
+
+    function ckmedico(address _md) private view returns (bool){
+        for (uint256 i=0; i < medici.length; i++){
+            if (medici[i] == _md){return true;}
+        }
+        return false;
     }
 }
