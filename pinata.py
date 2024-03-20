fbbd54af880283111a933a320d91ff6f457732dd
Marco Incipini <marco.incipini@gmail.com>
2024-03-20 10:29:59 +0100

    Prova setup

---
+++ pinata.py
@@ -0,0 +1,64 @@
+from brownie import GestioneADI, accounts, Contract
+import sys
+import requests
+import os
+from dotenv import load_dotenv
+import pprint
+
+def get_file_from_pinata(ipfs_hash):
+    url = f"https://gateway.pinata.cloud/ipfs/{ipfs_hash}"
+    headers = {'Authorization': f'Bearer {jwt_token}'}
+    
+    jwt_token = os.getenv('PINATA_JWT_TOKEN') # salvo in una variabile la chiave API necessaria per la richiesta a Pinata
+
+    try:
+        """
+        invia una richiesta GET all'URL specificato, inclusi gli header forniti, e memorizza la risposta nella variabile response. 
+        La risposta conterrà i dati restituiti dal server al quale è stata inviata la richiesta GET.
+        """
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Solleva un'eccezione se la richiesta HTTP non ha successo
+        
+        # Parse the JSON response
+        file_data = response.json()
+        
+        # estrazione del file originale dal json ottenuto, si può mettere qualsiasi cosa
+        original_file_content = file_data['content']
+        
+        return original_file_content  # restituisce il contenuto originale del file
+    except requests.exceptions.RequestException as e:
+        print("Errore durante la richiesta HTTP:", e) # controllo su eventuali errori della richiesta http
+        return None
+
+def upload_to_pinata(filepath):
+    jwt_token = os.getenv('PINATA_JWT_TOKEN') # salvo in una variabile la chiave API necessaria per la richiesta a Pinata
+
+    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+    headers = {'Authorization': f'Bearer {jwt_token}'}
+
+    with open(filepath, 'rb') as file:
+        """
+         Invia una richiesta POST all'URL specificato, inclusi i dati del file e gli header forniti, e memorizza 
+         la risposta nella variabile response. 
+         La risposta conterrà i dati restituiti dal server al quale è stata inviata la richiesta POST.
+        """
+        response = requests.post(url, files={'file': file}, headers=headers)
+        response_json = response.json() # restituisce il json associato al file caricato su Pinata
+
+        # Estrai l'hash del file caricato dal JSON di risposta
+        ipfs_hash = response_json.get('IpfsHash', None)
+
+        return ipfs_hash  # Restituisci l'hash del file caricato
+
+"""
+Questa funzione prende in input una cartella contentente diversi file e li carica uno per uno su Pinata, tramite chiamate a 
+funzione di upload_to_pinata.
+"""
+def upload_files_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            print(f"Uploading file: {file_path}")
+            response = upload_to_pinata(file_path)
+            print(f"IPFS Hash: {response['IpfsHash']}")
+            print()
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,7 +1,37 @@
 from brownie import accounts, network, Contract, GestioneADI
+import json
+import pinata
 
 # Devo sempre definire un metodo main
+# Il setup serve a sviluppare ul contratto da utilizzare ed inserire i dati iniziali.
 def main():
     contratto=GestioneADI.deploy(accounts[0],{'from':accounts[0]})
     #contratto.setPaziente(accounts[1],10,10)
+    
+    # Devo associare gli account ai vari ruoli
+    with open('scripts/pazienti.json','r') as file:
+        pazienti = json.load(file)
+    
+    for item in pazienti:
+        contratto.setPaziente(accounts[item['ID']],item['Latitudine'],item['Longitudine'],{'from':accounts[0]})
+    for item in range(6,8):
+        contratto.setMedico(accounts[item],{'from':accounts[0]})
+    for item in range(8,10):
+        contratto.setOperatore(accounts[item],{'from':accounts[0]})
+
+    # Istanziare un set di dati di prova per ogni struttura
+    with open('scripts/terapie.json','r') as file:
+        terapie = json.load(file)
+    for i in range(1,6):
+        dati= list(filter(lambda x:x["IDPaziente"]==i,terapie))
+        with open('temp.json','w') as file:
+            json.dump(dati,file)
+        try:
+            hash = pinata.upload_to_pinata('temp.json')
+        except:
+            print('errore')
+        print(hash)
+        contratto.setTerapia(accounts[i],hash,{'from':accounts[0]})
+
+
     return contratto
