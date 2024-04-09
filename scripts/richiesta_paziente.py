f9eff3caa538f876c40cb1dac68fc035d498ccdb
FedericoPaolucci <s1116751@studenti.univpm.it>
2024-04-09 14:24:42 +0200

    demo richiesta paziente + merge con main

--- scripts/richiesta_paziente.py
+++ scripts/richiesta_paziente.py
@@ -1,12 +1,11 @@
 from brownie import accounts, network, Contract, GestioneADI
-import json
-import pinata
-import setup
+import scripts.setup as set
 
 #demo: Aggiunge un paziente e lo stesso manda una richiesta all'ASUR per essere aggiunto al programma ADI
 def main():
     contratto=GestioneADI.deploy(accounts[0],{'from':accounts[0]})
-    setup.utenti(contratto)
+    set.utenti(contratto)
+    
     #sono impostati i dati del nuovo paziente:
     nuovo_paziente = {
         "ID": 10,
@@ -15,6 +14,12 @@ def main():
     }
     #viene aggiunto un accounts
     new_account = accounts.add()
+    print("E' stato creato un nuovo paziente: ", accounts[nuovo_paziente['ID']])
 
     #viene inviata una richiesta dal paziente
-    contratto.setRichieste(accounts[nuovo_paziente['ID']], nuovo_paziente['Latitudine'], nuovo_paziente['Longitudine'], {'from': accounts[nuovo_paziente['ID']]})
+    contratto.SetRichieste(accounts[nuovo_paziente['ID']], nuovo_paziente['Latitudine'], nuovo_paziente['Longitudine'], {'from': accounts[nuovo_paziente['ID']]})
+    print("Il paziente ", accounts[nuovo_paziente['ID']], " ha inviato una richiesta ADI.")
+
+    print("La lista delle richieste è:")
+    print(contratto.getRichieste())
+
