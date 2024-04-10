b1abff377bae0d5e6e66d80e23ae43bcdfa48009
Marco Incipini <marco.incipini@gmail.com>
2024-04-10 15:14:03 +0200

    Demo conferme completa

--- scripts/demoConferme.py
+++ scripts/demoConferme.py
@@ -33,9 +33,23 @@ def main():
         while not (operatore==8 or operatore==9 or operatore == 0):
             operatore = int(input("Inserisci ID dell'operatore (8 - 9): "))
 
-        
+    paziente = int(input("Inserisci ID del paziente (1-5): "))
+    while paziente>5 or paziente<0:
+        paziente = int(input("Inserisci ID del paziente (1-5): "))
+    while paziente!=0:
+        validazione = contratto.getValidazione({'from':accounts[paziente]})
+        print(validazione)
+        _id = input("Inserire ID della prestazione da confermare: ")
+        try:
+            contratto.confermaPaziente(_id,{'from':accounts[paziente]})
+        except Exception as ex:
+            print("Errore rilevato nella conferma: ", ex)
+        paziente = int(input("Inserisci ID del paziente (1-5): "))
+        while paziente>5 or paziente<0:
+            paziente = int(input("Inserisci ID del paziente (1-5): "))
 
     print(contratto.getConferme({'from':accounts[8]}))
+    print(contratto.getConferme({'from':accounts[9]}))
     print(contratto.getValidazione({'from':accounts[1]}))
     print(contratto.getValidazione({'from':accounts[2]}))
     print(contratto.getValidazione({'from':accounts[3]}))
